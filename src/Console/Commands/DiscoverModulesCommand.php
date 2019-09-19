34babeca2d753c5a9c4929d24c21daf4e5843453
LozovoyV <lozovoyv@gmail.com>
2019-09-19 16:53:26 +0300

    Add module discovering

---
+++ src/Console/Commands/DiscoverModulesCommand.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Core\Console\Commands;
+
+use Core\Tools\Modules\ModulesLister;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class DiscoverModulesCommand extends Command
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'modules:discover';
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'modules:discover';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Discover all OpxCMS installed modules';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $modules = ModulesLister::discoverModules(app());
+
+        if (empty($modules)) {
+            $this->info('Nothing to discover');
+        } else {
+            foreach ($modules as $module) {
+                $this->info('Discovered: ' . $module['module']);
+            }
+        }
+    }
+}
--- src/Tools/Modules/ModulesLister.php
+++ src/Tools/Modules/ModulesLister.php
@@ -10,20 +10,22 @@ class ModulesLister
     /**
      * Collect registration data from all modules.
      *
-     * @param  \Core\Foundation\Application  $app
-     * @param  int  $depth
-     * @param  string  $registerFileName
+     * @param Application $app
+     * @param int $depth
+     *
+     * @param string $registerFileName
      *
      * @return  array
      */
-    public static function getRegistrarData(Application $app, $depth = 3, $registerFileName = 'register.php')
+    public static function getRegistrarData(Application $app, $depth = 3, $registerFileName = 'register.php'): array
     {
         $data = [];
 
+        // Get local modules from `modules` folder.
         foreach (self::getRegistrarFiles($app, $depth, $registerFileName) as $file) {
             $data[] = [
                 'path' => $file->getPath(),
-                'data' =>require $file,
+                'data' => require $file,
             ];
         }
 
@@ -31,15 +33,56 @@ class ModulesLister
     }
 
     /**
-     * @param  \Core\Foundation\Application  $app
-     * @param  int $depth
-     * @param  string  $registerFileName
+     * @param Application $app
+     * @param int $depth
+     * @param string $registerFileName
      *
-     * @return  \Symfony\Component\Finder\Finder
+     * @return  Finder
      */
-    public static function getRegistrarFiles(Application $app, $depth = 3, $registerFileName = 'register.php')
+    public static function getRegistrarFiles(Application $app, $depth = 3, $registerFileName = 'register.php'): Finder
     {
+        return Finder::create()->depth('< ' . ($depth + 1))->name($registerFileName)->in($app->getModulesPath());
+    }
 
-        return Finder::create()->depth('< '.($depth + 1))->name($registerFileName)->in($app->getModulesPath());
+    /**
+     * Discover modules.
+     *
+     * @param Application $app
+     *
+     * @return  array
+     */
+    public static function discoverModules(Application $app): array
+    {
+        $installedPath = $app->basePath('composer/installed.json');
+
+        if (!file_exists($installedPath)) {
+            return;
+        }
+
+        $packages = json_decode(require $installedPath, true);
+
+        $modules = [];
+
+        $discovered = [];
+        $discoveredFileName = $app->storagePath('system/modules.php');
+
+        if (file_exists($discoveredFileName)) {
+            $discovered = require $discoveredFileName;
+        }
+
+        foreach ($packages as $package) {
+            if (isset($packag['extra']['opxcms']['module'])) {
+                $class = $package['extra']['opxcms']['module'];
+                $modules[$package['name']] = [
+                    'module' => $class,
+                    'path' => $class,
+                    'enabled' => $discovered[$package['name']] ?? true,
+                ];
+            }
+        }
+
+        file_put_contents($discoveredFileName, '<?php return ' . var_export($modules, true) . ';');
+
+        return $modules;
     }
 }
